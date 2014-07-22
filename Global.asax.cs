e7f7646cb89f2e07f3ef0df7bc1a9c466b32f60f
Popeye Zhong <zongsoft@gmail.com>
2014-07-22 11:27:18 +0800

    Modify the Global and .csproj files.

--- Global.asax.cs
+++ Global.asax.cs
@@ -11,7 +11,7 @@ namespace Zongsoft.Web.Launcher
 	{
 		protected void Application_Start()
 		{
-			Zongsoft.Plugins.Application.Start(Zongsoft.Plugins.Web.ApplicationContext.Current, null);
+			Zongsoft.Plugins.Application.Start(Zongsoft.Web.Plugins.ApplicationContext.Current, null);
 		}
 	}
 }
