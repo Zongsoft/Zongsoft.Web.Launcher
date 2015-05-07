17cf0c674a2e64c2a4763faebe07534caa5fadeb
Popeye Zhong <zongsoft@gmail.com>
2015-05-07 23:42:05 +0800

    修复启动与退出应用程序的逻辑问题。

--- Global.asax.cs
+++ Global.asax.cs
@@ -11,7 +11,18 @@ namespace Zongsoft.Web.Launcher
 	{
 		protected void Application_Start()
 		{
+			Zongsoft.Plugins.Application.Started += Application_Started;
 			Zongsoft.Plugins.Application.Start(Zongsoft.Web.Plugins.ApplicationContext.Current, null);
+		}
+
+		protected void Application_End(object sender, EventArgs e)
+		{
+			Zongsoft.Plugins.Application.Exit();
+		}
+
+		private void Application_Started(object sender, Zongsoft.Plugins.ApplicationEventArgs e)
+		{
+			Application["ApplicationContext"] = Zongsoft.Plugins.Application.Context;
 		}
 	}
 }
